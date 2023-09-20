8f35716787b15538086357f29be64c76cd586e1e
Alexey Polyakov <folsten@alfa.im>
2023-09-20 21:44:23 +0300

    first init

---
+++ src/components/header-main/header-main.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div class="container h-48">
+      <div class="flex justify-between">
+        <div class="h">weffwe</div>
+        <div class="wef">wqqw</div>
+      </div>
+      <div class="flex">fwe</div>
+      <div class="flex">fwe</div>
+    </div>
+  </header>
+</template>
---
+++ src/views/home-view.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <HeaderMain />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeaderMain from '../components/header-main/header-main.vue'
+</script>
