f524958c935311cbe0c257514522ed573c00de36
Alexey Polyakov <folsten@alfa.im>
2023-09-21 07:59:44 +0300

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
       center: true,
     },
     screens: {
-      '2xl': '1200px'
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
     },
     colors: {
       // > Primary colors
