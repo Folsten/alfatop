826b914997c9dabffd1168481cbfb79323531eaa
Alexey Polyakov <folsten@alfa.im>
2023-10-01 01:30:04 +0300

    BEM naming done for log-in page

--- src/pages/log-in/components/log-in-email-verification.vue
+++ src/pages/log-in/components/log-in-email-verification.vue
@@ -1,32 +1,32 @@
 <template>
   <div
-    class="register-start flex flex-col items-center flex-[2] justify-center w-full ml-5 mr-5 xs:ml-3 xs:mr-3"
+    class="log-in-email-verification flex flex-col items-center flex-[2] justify-center w-full ml-5 mr-5 xs:ml-3 xs:mr-3"
   >
     <div
-      class="register-start__main-container flex flex-col items-center w-full max-w-[500px] border rounded-lg bg-gray-50 p-10 xs:p-5"
+      class="log-in-email-verification__main-container flex flex-col items-center w-full max-w-[500px] border rounded-lg bg-gray-50 p-10 xs:p-5"
     >
-      <div class="register-start__title w-full text-left text-lg font-semibold text-gray-900">
+      <div class="log-in-email-verification__title w-full text-left text-lg font-semibold text-gray-900">
         Email verification code
       </div>
-      <div class="register-start__subtitle w-full text-left text-sm text-gray-500">
+      <div class="log-in-email-verification__subtitle w-full text-left text-sm text-gray-500">
         We have sent an email with a verification code to your email address. In order to complete
         the password reset process, please enter the confirmation code below.
       </div>
-      <div class="flex w-full justify-between mt-6">
-        <input type="text" class="input-text w-12 h-12" />
-        <input type="text" class="input-text w-12 h-12" />
-        <input type="text" class="input-text w-12 h-12" />
-        <input type="text" class="input-text w-12 h-12" />
-        <input type="text" class="input-text w-12 h-12" />
-        <input type="text" class="input-text w-12 h-12" />
+      <div class="log-in-email-verification__email-code-container flex w-full justify-between mt-6">
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
+        <input type="text" class="log-in-email-verification__email-code-input input-text w-12 h-12" />
       </div>
-      <button class="btn btn-indigo w-full h-10 mt-6 rounded text-sm">Proceed</button>
-      <div class="flex w-full text-gray-900 text-sm mt-4">
+      <button class="log-in-email-verification__btn-proceed btn btn-indigo w-full h-10 mt-6 rounded text-sm">Proceed</button>
+      <div class="log-in-email-verification__email-arrive flex w-full text-gray-900 text-sm mt-4">
         Email did not arrive?
-        <span class="text-gray-500 text-sm ml-1">Resend in 09:33</span>
+        <span class="log-in-email-verification__email-resend text-gray-500 text-sm ml-1">Resend in 09:33</span>
       </div>
-      <div class="text-sm mt-4 w-full">If you have not received that email, check spam folders.</div>
-      <router-link to="#" class="text-indigo-500 w-full text-sm mt-4">Log out</router-link>
+      <div class="log-in-email-verification__check-spam-folder-text text-sm mt-4 w-full">If you have not received that email, check spam folders.</div>
+      <router-link to="#" class="log-in-email-verification__log-out-text text-indigo-500 w-full text-sm mt-4">Log out</router-link>
     </div>
   </div>
 </template>
--- src/pages/log-in/components/log-in-password-changed.vue
+++ src/pages/log-in/components/log-in-password-changed.vue
@@ -1,20 +1,20 @@
 <template>
   <div
-    class="register-start flex flex-col items-center flex-[2] justify-center w-full ml-5 mr-5 xs:ml-3 xs:mr-3"
+    class="log-in-password-changed flex flex-col items-center flex-[2] justify-center w-full ml-5 mr-5 xs:ml-3 xs:mr-3"
   >
     <div
-      class="register-start__main-container flex flex-col items-center w-full max-w-[500px] border rounded-lg bg-gray-50 p-10 xs:p-5"
+      class="log-in-password-changed__main-container flex flex-col items-center w-full max-w-[500px] border rounded-lg bg-gray-50 p-10 xs:p-5"
     >
-      <SvgIcon name="icon-confirm" class="text-success-500 h-12 w-12 mb-7"/>
-      <div class="register-start__title w-full text-center text-lg font-semibold text-gray-900">
+      <SvgIcon name="icon-confirm" class="log-in-password-changed__icon-confirm text-success-500 h-12 w-12 mb-7"/>
+      <div class="log-in-password-changed__title w-full text-center text-lg font-semibold text-gray-900">
         Password was successfully changed
       </div>
-      <div class="register-start__subtitle w-full text-center text-sm text-gray-500">
+      <div class="log-in-password-changed__subtitle w-full text-center text-sm text-gray-500">
         Now you can Log in to your account with a new password
       </div>
-      <div class="flex mt-6 justify-between w-full">
-        <button class="btn btn-neutral text-sm w-fit h-10 pl-4 pr-4">Cancel</button>
-        <button class="btn btn-indigo text-sm w-fit h-10 pl-4 pr-4">Proceed to Log in</button>
+      <div class="log-in-password-changed__buttons-container flex mt-6 justify-between w-full">
+        <button class="log-in-password-changed__btn-cancel btn btn-neutral text-sm w-fit h-10 pl-4 pr-4">Cancel</button>
+        <button class="log-in-password-changed__btn-proceed btn btn-indigo text-sm w-fit h-10 pl-4 pr-4">Proceed to Log in</button>
       </div>
     </div>
   </div>
