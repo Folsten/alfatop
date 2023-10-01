e3b40ae71a038a24895956b31a81ac9ba30980c3
Alexey Polyakov <folsten@alfa.im>
2023-10-01 11:42:40 +0300

    account-sidebar update

--- src/pages/account/components/account-sidebar.vue
+++ src/pages/account/components/account-sidebar.vue
@@ -3,11 +3,22 @@
     class="sidebar flex flex-col justify-start items-start h-screen w-[66px] border border-gray-100 bg-gray-25 shadow-[4px_0px_8px_0px_rgba(0,0,0,0.02)]"
     :class="{ 'sidebar-open-core': isOpen }"
   >
-    <div class="cursor-pointer flex justify-center items-center w-16 h-16 mt-4 mb-6">
-      <SvgIcon name="icon-logo-no-text" class="w-8 h-8 flex" />
+    <div class="relative flex items-center w-full mt-4 mb-6">
+      <div class="cursor-pointer flex justify-center items-center w-16 h-16">
+        <SvgIcon name="icon-logo-no-text" class="transition-all w-8 h-8 flex" />
+      </div>
+      <Transition name="fade-007">
+        <div
+          v-if="isOpen"
+          @click="isOpen = !isOpen"
+          class="absolute right-0 flex justify-center items-center cursor-pointer p-2 hover:bg-gray-100 hover:rounded-lg"
+        >
+          <SvgIcon name="icon-sidebar-right" class="w-6 h-6 rotate-180 text-gray-700"></SvgIcon>
+        </div>
+      </Transition>
     </div>
     <div
-      :class="{ 'w-[366px] !pl-2': isOpen }"
+      :class="{ 'w-[278px] !pl-2': isOpen }"
       class="transition-all cursor-pointer flex justify-start items-center h-16 p-2 pl-3"
     >
       <div class="w-full flex p-2 hover:bg-gray-100 hover:rounded-lg">
@@ -18,7 +29,7 @@
       </div>
     </div>
     <div
-      :class="{ 'w-[366px] !pl-2': isOpen }"
+      :class="{ 'w-[278px] !pl-2': isOpen }"
       class="transition-all cursor-pointer flex justify-start items-center h-16 p-2 pl-3"
     >
       <div class="w-full flex p-2 hover:bg-gray-100 hover:rounded-lg">
@@ -29,7 +40,7 @@
       </div>
     </div>
     <div
-      :class="{ 'w-[366px] !pl-2': isOpen }"
+      :class="{ 'w-[278px] !pl-2': isOpen }"
       class="transition-all cursor-pointer flex justify-start items-center h-16 p-2 pl-3"
     >
       <div class="w-full flex p-2 hover:bg-gray-100 hover:rounded-lg">
@@ -44,7 +55,11 @@
       class="cursor-pointer flex justify-start items-center h-16 p-2 pl-2 mt-auto"
     >
       <div class="w-full flex p-2 hover:bg-gray-100 hover:rounded-lg">
-        <SvgIcon :class="{'sidebar-icon-revert': isOpen}" name="icon-sidebar-right" class="w-6 h-6 self-center text-gray-700" />
+        <SvgIcon
+          :class="{ 'rotate-180': isOpen }"
+          name="icon-sidebar-right"
+          class="w-6 h-6 self-center text-gray-700"
+        />
       </div>
     </div>
   </div>
@@ -62,7 +77,7 @@ let isOpen = ref(false)
 }
 
 .sidebar-open-core {
-  width: 400px !important;
+  width: 312px !important;
   padding-left: 16px;
   padding-right: 16px;
 }
@@ -70,9 +85,5 @@ let isOpen = ref(false)
 .sidebar-open-row {
   justify-content: flex-start;
   width: 100%;
-}
-
-.sidebar-icon-revert {
-  transform: rotate(180deg);
 }
 </style>
