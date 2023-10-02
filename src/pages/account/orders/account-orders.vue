bd207b431c46cd1ae15b7ecd5a84e57c4516ab11
Alexey Polyakov <folsten@alfa.im>
2023-10-02 22:04:46 +0300

    account-orders page full finished

--- src/pages/account/orders/account-orders.vue
+++ src/pages/account/orders/account-orders.vue
@@ -1,31 +1,31 @@
 <template>
-  <Header :isMenuVisible="false" class="hidden md:flex"/>
+  <Header :isMenuVisible="false" class="hidden md:flex" />
   <div class="w-full flex">
     <AccountSidebar />
     <div class="flex flex-col w-full">
       <div class="flex justify-end h-[66px] w-full items-center border-b border-gray-100 md:hidden">
-        <div class="flex items-center pr-6">
+        <div class="container w-full justify-end flex items-center pr-6">
           <SvgIcon name="icon-log-out" class="w-4 h-4 text-gray-700 mr-2" />
           <span class="text-sm text-gray-700 font-medium">Log out</span>
         </div>
       </div>
-      <div class="flex flex-col pl-6 pr-6 md:mt-10">
+      <div class="container flex flex-col pl-6 pr-6 md:mt-10">
         <div class="flex flex-col w-full mt-4">
           <div class="text-lg font-medium text-gray-900">Orders</div>
           <div class="text-sm text-gray-500">View all recent orders</div>
         </div>
         <div
-          class="flex flex-col w-full min-h-[190px] items-center justify-center bg-gray-50 border border-gray-100 mt-10 rounded-lg"
+          class="flex flex-col w-full min-h-[190px] items-center justify-center bg-gray-50 border border-gray-100 mt-10 p-8 rounded-lg"
         >
           <SvgIcon name="icon-paper-in-eclipse" class="w-16 h-16" />
           <div class="text-sm text-gray-900 font-semibold text-center mt-2">No orders made yet</div>
           <div class="mt-2 text-center">There are no orders to be displayed here.</div>
         </div>
-        <div class="w-full flex justify-center items-center bg-gray-50 rounded-lg pb-10 pt-10 min-h-[304px] mt-20 mb-10 pl-16 pr-16 md:mt-10">
-          <Newsletter
-            :is-ultra-responsive="true"
-            class="xl:flex-wrap"
-          />
+        <AccountOrdersTable/>
+        <div
+          class="w-full flex justify-center items-center bg-gray-50 rounded-lg pb-10 pt-10 min-h-[304px] mt-20 mb-10 pl-16 pr-16 md:mt-10 sm:pl-8 sm:pr-8"
+        >
+          <Newsletter :is-ultra-responsive="true" class="xl:flex-wrap" />
         </div>
         <Footer :is-ultra-responsive="true" class="w-full mb-5" />
       </div>
@@ -37,5 +37,6 @@ import AccountSidebar from '@/pages/account/components/account-sidebar.vue'
 import SvgIcon from '~/svg-icon/svg-icon.vue'
 import Newsletter from '~/newsletter/newsletter.vue'
 import Footer from '~/footer/footer.vue'
-import Header from "~/header/header.vue";
+import Header from '~/header/header.vue'
+import AccountOrdersTable from "@/pages/account/orders/components/account-orders-table.vue";
 </script>
---
+++ src/pages/account/orders/components/account-orders-table-item.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- XL+ VERSION -->
+  <div class="flex w-full items-start h-[72px] border-b border-b-gray-100 pt-2.5 pb-2.5 xl:hidden">
+    <div class="flex w-[20%] text-gray-900 text-md font-medium pr-4 pl-4">12345678910</div>
+    <div class="flex w-[35%] items-center pr-4">
+      <img src="../../../../assets/images/playstation.png" class="h-10 w-10 rounded-lg" />
+      <div class="flex flex-col w-full justify-start ml-2">
+        <div class="flex w-full">
+          <div class="text-md font-medium text-gray-900">Amazon.es Spain</div>
+          <div class="text-sm font-medium text-gray-400 ml-5">+3 more</div>
+        </div>
+        <div class="text-sm text-gray-500">20 EUR</div>
+      </div>
+    </div>
+    <div class="flex w-[20%] flex-col text-center pr-4">
+      <div class="text-gray-900 text-sm font-medium text-right">8.56411352 USDT</div>
+      <div class="text-xs text-gray-500 font-medium text-right">8456.56 EUR</div>
+    </div>
+    <div class="flex w-[15%] ml-auto items-center">
+      <SvgIcon name="icon-lightning-blue" class="w-4 h-4" />
+      <div class="text-sm text-gray-900 font-medium pl-2">Paid</div>
+    </div>
+  </div>
+  <!-- XL- VERSION -->
+  <div class="hidden flex-col rounded-md border border-gray-100 pt-4 pb-4 pl-3 pr-5 xl:flex">
+    <div class="flex border-b border-gray-100 pb-2">
+      <div class="mr-1 text-gray-900 text-sm font-medium sm:text-xs">Order:</div>
+      <router-link to="#" class="text-sm font-medium text-indigo-500 sm:text-xs">123456789109876543212345</router-link>
+    </div>
+    <div class="flex items-center border-b border-gray-100 mt-2 pb-2">
+      <img src="@/assets/images/playstation.png" class="w-8 h-8 rounded-lg mr-2">
+      <div class="text-gray-900 font-medium text-sm sm:text-xs">Amazon.es Spain</div>
+      <div class="text-gray-400 font-medium text-sm ml-auto sm:text-xs">+3 more</div>
+    </div>
+    <div class="text-gray-900 font-medium text-sm mb-2 mt-2 sm:text-xs">
+      8.56411352 USDT TRC-20
+    </div>
+    <div class="flex justify-between">
+      <div class="text-sm text-gray-500 font-medium sm:text-xs">8456.56 EUR</div>
+      <div class="flex items-center">
+        <SvgIcon name="icon-lightning-yellow" class="w-4 h-4 sm:w-3 sm:h-3"/>
+        <div class="ml-2 font-semibold text-gray-900 text-sm sm:text-xs sm:ml-1">NEW</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import SvgIcon from "~/svg-icon/svg-icon.vue";
+</script>
