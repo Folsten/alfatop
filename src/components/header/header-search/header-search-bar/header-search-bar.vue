9c8a86e4beb68a2f102daf2bd8be7ce6f4741a02
Alexey Polyakov <folsten@alfa.im>
2023-10-06 12:49:58 +0300

    #8 fixed

--- src/components/header/header-search/header-search-bar/header-search-bar.vue
+++ src/components/header/header-search/header-search-bar/header-search-bar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center pr-3 pt-2 pb-2 w-full">
     <SvgIcon name="icon-loop" class="w-5 h-5 mr-2" />
-    <input type="text" class="w-full relative items-center flex pr-1" placeholder="Search..." />
+    <input type="text" class="text-gray-900 w-full relative items-center flex pr-1" placeholder="Search..." />
   </div>
 </template>
 
