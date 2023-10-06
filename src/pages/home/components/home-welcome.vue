7551e4e77f47ffe798001ea6547f291cf3fbbb43
Alexey Polyakov <folsten@alfa.im>
2023-10-06 13:20:10 +0300

    +

--- src/pages/home/components/home-welcome.vue
+++ src/pages/home/components/home-welcome.vue
@@ -8,34 +8,34 @@
     </div>
     <div class="home-welcome__wrapper-for-mobile-and-gift-cards flex sm:flex-wrap">
       <div
-        class="home-welcome__wrapper-for-mobile-card flex w-full items-center mr-5 rounded-lg overflow-hidden h-[224px] sm:mb-5 lg:mr-2"
+        class="home-welcome__wrapper-for-mobile-card flex w-full items-center mr-5 rounded-lg overflow-hidden h-[224px] sm:mb-5 sm:mr-0 lg:mr-2"
       >
         <div class="home-welcome__mobile-card h-full w-full">
           <div
             class="home-welcome__mobile-card-container p-8 flex flex-col justify-center h-full md:p-4"
           >
             <span
-              class="home-welcome__mobile-card-title text-white leading-10 sm:text-center text-dh4 md:text-mh4"
+              class="home-welcome__mobile-card-title text-white leading-10 sm:text-center font-semibold text-dh4 md:text-mh4"
               >Recharge your mobile <br class="lg:hidden" />with crypto</span
             >
             <router-link
               to="#"
               tag="button"
-              class="home-welcome__mobile-card-btn btn btn-neutral w-40 h-10 mt-5 sm:w-full"
+              class="home-welcome__mobile-card-btn btn btn-neutral font-medium text-gray-700 font-medium w-40 h-10 mt-5 sm:w-full"
               >Browse products
             </router-link>
           </div>
         </div>
       </div>
       <div
-        class="home-welcome__wrapper-gifts-card flex w-full items-center ml-5 rounded-lg overflow-hidden h-[224px] sm:mt-5 lg:ml-2"
+        class="home-welcome__wrapper-gifts-card flex w-full items-center ml-5 rounded-lg overflow-hidden h-[224px] sm:mt-5 sm:ml-0 lg:ml-2"
       >
         <div class="home-welcome__gifts-card h-full w-full">
           <div
             class="home-welcome__gifts-card-container p-8 flex flex-col h-full justify-center md:p-4"
           >
             <span
-              class="home-welcome__gifts-card-title text-white leading-10 sm:text-center text-dh4 md:text-mh4"
+              class="home-welcome__gifts-card-title text-white leading-10 font-semibold sm:text-center text-dh4 md:text-mh4"
               >Shop exclusive gift cards <br class="lg:hidden" />and more</span
             >
             <div class="home-welcome__wrapper-gifts-card-2 flex mt-5 sm:flex-wrap">
@@ -46,14 +46,14 @@
                 />
                 <input
                   type="tel"
-                  class="home-welcome__gifts-card-phone-input h-10 w-full text-gray-700 flex justify-center items-start bg-gray-25 rounded-md pl-10"
+                  class="home-welcome__gifts-card-phone-input h-10 w-full text-gray-700 flex justify-center items-start bg-gray-25 rounded-md pl-10 placeholder:font-medium"
                   placeholder="+375"
                 />
               </div>
               <router-link
                 to="#"
                 tag="button"
-                class="home-welcome__gifts-card-btn btn btn-orange w-20 h-10 pl-3 pr-3 ml-4 rounded-md lg:text-sm sm:w-full sm:ml-0"
+                class="home-welcome__gifts-card-btn btn btn-orange font-medium w-20 h-10 pl-3 pr-3 ml-4 rounded-md lg:text-sm sm:w-full sm:ml-0"
                 >Top up
               </router-link>
             </div>
